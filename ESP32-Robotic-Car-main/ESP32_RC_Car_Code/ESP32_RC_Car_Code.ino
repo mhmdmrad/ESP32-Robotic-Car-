55fe3c8d973c59d9ea1491f728fe74d6625e34f2
MMinusOne <michael.business.lb@proton.me>
2025-03-02 00:14:05 +0200

    Optimizing Speed control

--- ESP32-Robotic-Car-main/ESP32_RC_Car_Code/ESP32_RC_Car_Code.ino
+++ ESP32-Robotic-Car-main/ESP32_RC_Car_Code/ESP32_RC_Car_Code.ino
@@ -1,44 +1,45 @@
-//esp32 board version must be 2.0.11
-//otherwise the code will not compile
+// esp32 board version must be 2.0.11
+// otherwise the code will not compile
 
 #include "BluetoothSerial.h"
 #include <Arduino.h>
 BluetoothSerial serialBT;
 
-//Bluetooth signal Store in this variable
+// Bluetooth signal Store in this variable
 char btSignal;
 
-//initial Speed
+// initial Speed
 int Speed = 100;
 
-//declare channel for pwm Output
+// declare channel for pwm Output
 #define R 0
 #define L 1
 
-//PWM Pin for Controlling the speed
+// PWM Pin for Controlling the speed
 int enA = 5;
 int enB = 23;
 
-//motor controlling pin
+// motor controlling pin
 int IN1 = 22;
 int IN2 = 21;
 int IN3 = 19;
 int IN4 = 18;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
-  //Bluetooth Name
+  // Bluetooth Name
   serialBT.begin("Mrad");
 
-  //output pin declare
+  // output pin declare
   pinMode(enA, OUTPUT);
   pinMode(enB, OUTPUT);
 
   // Setup PWM channels
-  ledcSetup(R, 5000, 8);  // Channel 0 for Motor A, 5 kHz frequency, 8-bit resolution
+  ledcSetup(R, 5000, 8); // Channel 0 for Motor A, 5 kHz frequency, 8-bit resolution
   ledcAttachPin(enA, R);
-  ledcSetup(L, 5000, 8);  // Channel 0 for Motor A, 5 kHz frequency, 8-bit resolution
+  ledcSetup(L, 5000, 8); // Channel 0 for Motor A, 5 kHz frequency, 8-bit resolution
   ledcAttachPin(enB, L);
 
   pinMode(IN1, OUTPUT);
@@ -46,64 +47,52 @@ void setup() {
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
 
-  //Intial State of Car
+  // Intial State of Car
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, LOW);
 }
 
-void loop() {
+void loop()
+{
 
-  while (serialBT.available()) {
+  while (serialBT.available())
+  {
     btSignal = serialBT.read();
-    //Serial.println(btSignal);
+    // Serial.println(btSignal);
 
-    if (btSignal == '0') Speed = 100;
-    if (btSignal == '1') Speed = 110;
-    if (btSignal == '2') Speed = 120;
-    if (btSignal == '3') Speed = 130;
-    if (btSignal == '4') Speed = 140;
-    if (btSignal == '5') Speed = 150;
-    if (btSignal == '6') Speed = 180;
-    if (btSignal == '7') Speed = 200;
-    if (btSignal == '8') Speed = 220;
-    if (btSignal == '9') Speed = 240;
-    if (btSignal == 'q') Speed = 255;
+    if (btSignal >= '0' && btSignal <= '9')
+      Speed = 100 + (btSignal - '0') * 20;
+    else if (btSignal == 'q')
+      Speed = 255;
 
-//to see the incoming signal in serial monitor
+    // to see the incoming signal in serial monitor
     Serial.println(btSignal);
-    
-  //backward
-    if (btSignal == 'B') {
+    switch (btSignal)
+    {
+    case 'B':
       backward();
-    }
-
-  //forward
-    else if (btSignal == 'F') {
+      break;
+    case 'F':
       forward();
-    }
-
-  //LEFT
-    else if (btSignal == 'L') {
+      break;
+    case 'L':
       left();
-    }
-
-  //RIGHT
-    else if (btSignal == 'R') {
+      break;
+    case 'R':
       right();
-    }
-
-  //STOP
-    else if (btSignal == 'S') {
+      break;
+    case 'S':
       stop();
+      break;
     }
   }
 }
 
-
-//function for control motor
-void backward() {
+// function for control motor
+void backward()
+{
   ledcWrite(R, Speed);
   ledcWrite(L, Speed);
 
@@ -111,9 +100,10 @@ void backward() {
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  }
+}
 
-void forward() {
+void forward()
+{
   ledcWrite(R, Speed);
   ledcWrite(L, Speed);
 
@@ -123,7 +113,8 @@ void forward() {
   digitalWrite(IN4, HIGH);
 }
 
-void left() {
+void left()
+{
   ledcWrite(R, Speed);
   ledcWrite(L, Speed);
 
@@ -133,7 +124,8 @@ void left() {
   digitalWrite(IN4, LOW);
 }
 
-void right() {
+void right()
+{
   ledcWrite(R, Speed);
   ledcWrite(L, Speed);
 
@@ -143,12 +135,13 @@ void right() {
   digitalWrite(IN4, HIGH);
 }
 
-void stop() {
-    ledcWrite(R, Speed);
-    ledcWrite(L, Speed);
+void stop()
+{
+  ledcWrite(R, Speed);
+  ledcWrite(L, Speed);
 
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, LOW);
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, LOW);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
